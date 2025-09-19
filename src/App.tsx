2086442a3f19fb2f5fc4ab631966582236c5dc23
Stephen DeLorme <stephen@d.elor.me>
2025-09-19 10:15:19 -0700

    styling home

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,24 @@ import React from 'react'
 import { BuiButtonReact as BuiButton } from '@sbddesign/bui-ui/react'
 import ArtAndLogo from './components/ArtAndLogo'
 import './App.css'
+import liberteImage from '../public/liberte.png';
 
 function App() {
   return (
-    <div className="freedom-home">
-      <ArtAndLogo />
-      <div className="buttons-container">
+    <div className="text-center max-w-md mx-auto flex flex-col gap-6 w-full items-center justify-center h-full p-6">
+      <img src={liberteImage} alt="Liberté" className="w-full h-auto mb-8" />
+      <div className="flex flex-col mx-auto max-w-[320px] gap-6 items-center w-full h-ful">
         <BuiButton 
           label="Get Started"
           styleType="filled"
-          data-name="Button" 
-          data-node-id="7241:7340"
-          className="get-started-button"
+          size="large"
+          wide
         />
         <BuiButton 
           label="Restore wallet"
           styleType="outline"
-          data-name="Button" 
-          data-node-id="7241:7341"
-          className="restore-wallet-button"
+          size="large"
+          wide
         />
       </div>
     </div>
